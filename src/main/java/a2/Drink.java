0a33e14b7b54e2c2a545b30f8f54fbc51c33cd43
nasirpi <pi.nasir@mail.utoronto.ca>
2019-03-23 20:05:53 +0100

    Implement the Modify Order functionality

--- src/main/java/a2/Drink.java
+++ src/main/java/a2/Drink.java
@@ -3,18 +3,24 @@ package a2;
 public class Drink {
     private String name;
     private int quantity;
+    private int drinkNum;
+    private static int drinkCount = 1;
 
     public Drink(String name, int quantity) {
         this.name = name;
         this.quantity = quantity;
+        this.drinkNum = drinkCount;
+        drinkCount += 1;
+        if (drinkCount > 1000) {
+            drinkCount = 1;
+        }
     }
 
     @Override
     public String toString() {
-        return "Drink{" +
-                "name='" + name + '\'' +
-                ", quantity=" + quantity +
-                '}';
+        return "Drink #" + drinkNum + " ---> " +
+                "name=" + name + "\n" +
+                "quantity=" + quantity + "\n";
     }
 
     public String getName() {
@@ -31,5 +37,9 @@ public class Drink {
 
     public void setQuantity(int quantity) {
         this.quantity = quantity;
+    }
+
+    public int getDrinkNum() {
+        return drinkNum;
     }
 }
